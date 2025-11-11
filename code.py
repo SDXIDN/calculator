86da7ac99dead78862f88aab2c383ee589c407d8
SDXIND <vasyacsgoprusynko@gmail.com>
2025-11-11 18:14:41 +0200

    upd file

---
+++ code.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def add(self, a, b):
+        # TODO: implement addition
+        raise NotImplementedError
+
+    def subtract(self, a, b):
+        # TODO: implement subtraction
+        raise NotImplementedError
+
+    def multiply(self, a, b):
+        # TODO: implement multiplication
+        raise NotImplementedError
+
+    def divide(self, a, b):
+        # TODO: implement division
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    print("Team calculator ready!")
